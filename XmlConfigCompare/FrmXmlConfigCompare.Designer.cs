a3894993914a9bc4df9818465e4f611216268578
Stanislav Mursaev <mursaev@sqlmaze.com>
2018-04-20 17:53:44 +0300

    v2

--- XmlConfigCompare/FrmXmlConfigCompare.Designer.cs
+++ XmlConfigCompare/FrmXmlConfigCompare.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbProd = new System.Windows.Forms.TextBox();
-            this.tbUAT = new System.Windows.Forms.TextBox();
+            this.tbConfig1 = new System.Windows.Forms.TextBox();
+            this.tbConfig2 = new System.Windows.Forms.TextBox();
             this.tbResult = new System.Windows.Forms.TextBox();
-            this.btnSelectProd = new System.Windows.Forms.Button();
-            this.btnSelectUAT = new System.Windows.Forms.Button();
+            this.btnSelectConfig1 = new System.Windows.Forms.Button();
+            this.btnSelectConfig2 = new System.Windows.Forms.Button();
             this.btnSelectResult = new System.Windows.Forms.Button();
             this.btnCompare = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
@@ -41,61 +41,64 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSelectConfig3 = new System.Windows.Forms.Button();
+            this.tbConfig3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // tbProd
+            // tbConfig1
             // 
-            this.tbProd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.tbConfig1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbProd.Location = new System.Drawing.Point(79, 12);
-            this.tbProd.Name = "tbProd";
-            this.tbProd.Size = new System.Drawing.Size(260, 20);
-            this.tbProd.TabIndex = 0;
+            this.tbConfig1.Location = new System.Drawing.Point(79, 12);
+            this.tbConfig1.Name = "tbConfig1";
+            this.tbConfig1.Size = new System.Drawing.Size(260, 20);
+            this.tbConfig1.TabIndex = 0;
             // 
-            // tbUAT
+            // tbConfig2
             // 
-            this.tbUAT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.tbConfig2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbUAT.Location = new System.Drawing.Point(79, 43);
-            this.tbUAT.Name = "tbUAT";
-            this.tbUAT.Size = new System.Drawing.Size(260, 20);
-            this.tbUAT.TabIndex = 1;
+            this.tbConfig2.Location = new System.Drawing.Point(79, 43);
+            this.tbConfig2.Name = "tbConfig2";
+            this.tbConfig2.Size = new System.Drawing.Size(260, 20);
+            this.tbConfig2.TabIndex = 1;
             // 
             // tbResult
             // 
             this.tbResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbResult.Location = new System.Drawing.Point(79, 72);
+            this.tbResult.Location = new System.Drawing.Point(79, 101);
             this.tbResult.Name = "tbResult";
             this.tbResult.Size = new System.Drawing.Size(260, 20);
             this.tbResult.TabIndex = 2;
             // 
-            // btnSelectProd
+            // btnSelectConfig1
             // 
-            this.btnSelectProd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectProd.Location = new System.Drawing.Point(345, 12);
-            this.btnSelectProd.Name = "btnSelectProd";
-            this.btnSelectProd.Size = new System.Drawing.Size(26, 23);
-            this.btnSelectProd.TabIndex = 3;
-            this.btnSelectProd.Text = "...";
-            this.btnSelectProd.UseVisualStyleBackColor = true;
-            this.btnSelectProd.Click += new System.EventHandler(this.btnSelectProd_Click);
+            this.btnSelectConfig1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectConfig1.Location = new System.Drawing.Point(345, 12);
+            this.btnSelectConfig1.Name = "btnSelectConfig1";
+            this.btnSelectConfig1.Size = new System.Drawing.Size(26, 23);
+            this.btnSelectConfig1.TabIndex = 3;
+            this.btnSelectConfig1.Text = "...";
+            this.btnSelectConfig1.UseVisualStyleBackColor = true;
+            this.btnSelectConfig1.Click += new System.EventHandler(this.btnSelectConfig1_Click);
             // 
-            // btnSelectUAT
+            // btnSelectConfig2
             // 
-            this.btnSelectUAT.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectUAT.Location = new System.Drawing.Point(345, 41);
-            this.btnSelectUAT.Name = "btnSelectUAT";
-            this.btnSelectUAT.Size = new System.Drawing.Size(26, 23);
-            this.btnSelectUAT.TabIndex = 4;
-            this.btnSelectUAT.Text = "...";
-            this.btnSelectUAT.UseVisualStyleBackColor = true;
-            this.btnSelectUAT.Click += new System.EventHandler(this.btnSelectUAT_Click);
+            this.btnSelectConfig2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectConfig2.Location = new System.Drawing.Point(345, 41);
+            this.btnSelectConfig2.Name = "btnSelectConfig2";
+            this.btnSelectConfig2.Size = new System.Drawing.Size(26, 23);
+            this.btnSelectConfig2.TabIndex = 4;
+            this.btnSelectConfig2.Text = "...";
+            this.btnSelectConfig2.UseVisualStyleBackColor = true;
+            this.btnSelectConfig2.Click += new System.EventHandler(this.btnSelectConfig2_Click);
             // 
             // btnSelectResult
             // 
             this.btnSelectResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectResult.Location = new System.Drawing.Point(345, 70);
+            this.btnSelectResult.Location = new System.Drawing.Point(345, 99);
             this.btnSelectResult.Name = "btnSelectResult";
             this.btnSelectResult.Size = new System.Drawing.Size(26, 23);
             this.btnSelectResult.TabIndex = 5;
@@ -105,7 +108,7 @@
             // 
             // btnCompare
             // 
-            this.btnCompare.Location = new System.Drawing.Point(79, 98);
+            this.btnCompare.Location = new System.Drawing.Point(79, 127);
             this.btnCompare.Name = "btnCompare";
             this.btnCompare.Size = new System.Drawing.Size(75, 23);
             this.btnCompare.TabIndex = 6;
@@ -116,7 +119,7 @@
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(12, 126);
+            this.lblStatus.Location = new System.Drawing.Point(12, 153);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(116, 13);
             this.lblStatus.TabIndex = 7;
@@ -135,44 +138,79 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Prod config";
+            this.label1.Text = "Config 1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 46);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 9;
-            this.label2.Text = "UAT config";
+            this.label2.Text = "Config 2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Location = new System.Drawing.Point(12, 104);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(23, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Diff";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Config 3";
+            this.label4.Visible = false;
+            // 
+            // btnSelectConfig3
+            // 
+            this.btnSelectConfig3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectConfig3.Location = new System.Drawing.Point(345, 70);
+            this.btnSelectConfig3.Name = "btnSelectConfig3";
+            this.btnSelectConfig3.Size = new System.Drawing.Size(26, 23);
+            this.btnSelectConfig3.TabIndex = 12;
+            this.btnSelectConfig3.Text = "...";
+            this.btnSelectConfig3.UseVisualStyleBackColor = true;
+            this.btnSelectConfig3.Visible = false;
+            this.btnSelectConfig3.Click += new System.EventHandler(this.btnSelectConfig3_Click);
+            // 
+            // tbConfig3
+            // 
+            this.tbConfig3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbConfig3.Location = new System.Drawing.Point(79, 72);
+            this.tbConfig3.Name = "tbConfig3";
+            this.tbConfig3.Size = new System.Drawing.Size(260, 20);
+            this.tbConfig3.TabIndex = 11;
+            this.tbConfig3.Visible = false;
+            // 
             // FrmXmlConfigCompare
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 148);
+            this.ClientSize = new System.Drawing.Size(383, 178);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSelectConfig3);
+            this.Controls.Add(this.tbConfig3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnCompare);
             this.Controls.Add(this.btnSelectResult);
-            this.Controls.Add(this.btnSelectUAT);
-            this.Controls.Add(this.btnSelectProd);
+            this.Controls.Add(this.btnSelectConfig2);
+            this.Controls.Add(this.btnSelectConfig1);
             this.Controls.Add(this.tbResult);
-            this.Controls.Add(this.tbUAT);
-            this.Controls.Add(this.tbProd);
+            this.Controls.Add(this.tbConfig2);
+            this.Controls.Add(this.tbConfig1);
             this.Name = "FrmXmlConfigCompare";
             this.Text = "Xml Config Compare";
             this.ResumeLayout(false);
@@ -182,11 +220,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox tbProd;
-        private System.Windows.Forms.TextBox tbUAT;
+        private System.Windows.Forms.TextBox tbConfig1;
+        private System.Windows.Forms.TextBox tbConfig2;
         private System.Windows.Forms.TextBox tbResult;
-        private System.Windows.Forms.Button btnSelectProd;
-        private System.Windows.Forms.Button btnSelectUAT;
+        private System.Windows.Forms.Button btnSelectConfig1;
+        private System.Windows.Forms.Button btnSelectConfig2;
         private System.Windows.Forms.Button btnSelectResult;
         private System.Windows.Forms.Button btnCompare;
         private System.Windows.Forms.Label lblStatus;
@@ -195,6 +233,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSelectConfig3;
+        private System.Windows.Forms.TextBox tbConfig3;
     }
 }
 
--- XmlConfigCompare/FrmXmlConfigCompare.cs
+++ XmlConfigCompare/FrmXmlConfigCompare.cs
@@ -15,18 +15,25 @@
             InitializeComponent();
         }
 
-        private void btnSelectProd_Click(object sender, EventArgs e)
+        private void btnSelectConfig1_Click(object sender, EventArgs e)
         {
-            openFileDialog1.FileName = tbProd.Text;
+            openFileDialog1.FileName = tbConfig1.Text;
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
-                tbProd.Text = openFileDialog1.FileName;
+                tbConfig1.Text = openFileDialog1.FileName;
         }
 
-        private void btnSelectUAT_Click(object sender, EventArgs e)
+        private void btnSelectConfig2_Click(object sender, EventArgs e)
         {
-            openFileDialog1.FileName = tbUAT.Text;
+            openFileDialog1.FileName = tbConfig2.Text;
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
-                tbUAT.Text = openFileDialog1.FileName;
+                tbConfig2.Text = openFileDialog1.FileName;
+        }
+
+        private void btnSelectConfig3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = tbConfig3.Text;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                tbConfig3.Text = openFileDialog1.FileName;
         }
 
         private void btnSelectResult_Click(object sender, EventArgs e)
@@ -36,11 +43,11 @@
                 tbResult.Text = saveFileDialog1.FileName;
         }
 
-        readonly List<string> keyAttributes = new List<string> { "name", "key", "ref", "path", "assembly" };
+        readonly List<string> keyAttributes = new List<string> { "name", "key", "ref", "path", "assembly", "path", "fileextension" };
 
         private class NodeInfo
         {
-            public string Name { get; set; }
+            //public string Name { get; set; }
             public string Value { get; set; }
             public Dictionary<string, string> Attributes { get; set; }
         }
@@ -48,15 +55,15 @@
         private class CompareAttrResult
         {
             public string Attribute { get; set; }
-            public string ValueOnProd { get; set; }
-            public string ValueOnUAT { get; set; }
+            public string ValueInConfig1 { get; set; }
+            public string ValueInConfig2 { get; set; }
         }
 
         private class CompareResult
         {
-            public List<string> MissedOnProd { get; set; }
-            public List<string> OnlyOnProd { get; set; }
-            public List<string> ChangedOnProd { get; set; }
+            public List<string> MissedInConfig1 { get; set; }
+            public List<string> OnlyInConfig1 { get; set; }
+            public List<string> ChangedInConfig1 { get; set; }
             public Dictionary<string, List<CompareAttrResult>> Changes { get; set; }
         }
 
@@ -76,7 +83,7 @@
 
             if (!values.ContainsKey(name)) values.Add(name, new NodeInfo());
 
-            values[name].Name = node.Name.LocalName;
+            //values[name].Name = node.Name.LocalName;
             values[name].Value = node.Value;
             values[name].Attributes = node.Attributes().ToDictionary(a => a.Name.LocalName.ToLower(), a => a.Value);
 
@@ -106,39 +113,39 @@
             return string.Equals(TrimSpaces(value1), TrimSpaces(value2));
         }
 
-        private CompareResult Compare(Dictionary<string, NodeInfo> prod, Dictionary<string, NodeInfo> uat)
+        private CompareResult Compare(Dictionary<string, NodeInfo> config1, Dictionary<string, NodeInfo> config2)
         {
             var result = new CompareResult
             {
-                OnlyOnProd = prod.Keys.Except(uat.Keys).ToList(),
-                MissedOnProd = uat.Keys.Except(prod.Keys).ToList(),
-                ChangedOnProd = new List<string>(),
+                OnlyInConfig1 = config1.Keys.Except(config2.Keys).ToList(),
+                MissedInConfig1 = config2.Keys.Except(config1.Keys).ToList(),
+                ChangedInConfig1 = new List<string>(),
                 Changes = new Dictionary<string, List<CompareAttrResult>>()
             };
 
-            foreach (var key in prod.Where(kvp => prod.Keys.Intersect(uat.Keys).Contains(kvp.Key)).Select(kvp => kvp.Key))
+            foreach (var key in config1.Where(kvp => config1.Keys.Intersect(config2.Keys).Contains(kvp.Key)).Select(kvp => kvp.Key))
             {
-                var vp = prod[key].Attributes;
-                var vu = uat[key].Attributes;
+                var vp = config1[key].Attributes;
+                var vu = config2[key].Attributes;
 
-                var attrOnlyOnProd = (vp.Where(kvp => vp.Keys.Except(vu.Keys).Contains(kvp.Key))
+                var attrOnlyInConfig1 = (vp.Where(kvp => vp.Keys.Except(vu.Keys).Contains(kvp.Key))
                     .Select(kvp => kvp.Key)
                     .Where(attrKey => !keyAttributes.Contains(attrKey))
                     .Select(attrKey => new CompareAttrResult
                         {
                             Attribute = attrKey,
-                            ValueOnProd = TrimSpaces(vp[attrKey]),
-                            ValueOnUAT = null
+                            ValueInConfig1 = TrimSpaces(vp[attrKey]),
+                            ValueInConfig2 = null
                         })).ToList();
                 
-                var attrMissedOnProd = (vu.Where(kvp => vu.Keys.Except(vp.Keys).Contains(kvp.Key))
+                var attrMissedInConfig1 = (vu.Where(kvp => vu.Keys.Except(vp.Keys).Contains(kvp.Key))
                     .Select(kvp => kvp.Key)
                     .Where(attrKey => !keyAttributes.Contains(attrKey))
                     .Select(attrKey => new CompareAttrResult
                         {
                             Attribute = attrKey,
-                            ValueOnProd = null,
-                            ValueOnUAT = TrimSpaces(vu[attrKey])
+                            ValueInConfig1 = null,
+                            ValueInConfig2 = TrimSpaces(vu[attrKey])
                         })).ToList();
 
                 var attrBothCompared = (vp.Where(kvp => vp.Keys.Intersect(vu.Keys)
@@ -148,20 +155,20 @@
                     .Select(attrKey => new CompareAttrResult
                         {
                             Attribute = attrKey,
-                            ValueOnProd = TrimSpaces(vp[attrKey]),
-                            ValueOnUAT = TrimSpaces(vu[attrKey])
+                            ValueInConfig1 = TrimSpaces(vp[attrKey]),
+                            ValueInConfig2 = TrimSpaces(vu[attrKey])
                         })).ToList();
                 
-                if (attrOnlyOnProd.Any() || attrMissedOnProd.Any() || attrBothCompared.Any())
+                if (attrOnlyInConfig1.Any() || attrMissedInConfig1.Any() || attrBothCompared.Any())
                 {
-                    if (!result.ChangedOnProd.Contains(key))
+                    if (!result.ChangedInConfig1.Contains(key))
                     {
-                        result.ChangedOnProd.Add(key);
+                        result.ChangedInConfig1.Add(key);
                         result.Changes.Add(key, new List<CompareAttrResult>());
                     }
 
-                    result.Changes[key].AddRange(attrOnlyOnProd);
-                    result.Changes[key].AddRange(attrMissedOnProd);
+                    result.Changes[key].AddRange(attrOnlyInConfig1);
+                    result.Changes[key].AddRange(attrMissedInConfig1);
                     result.Changes[key].AddRange(attrBothCompared);
                 }
             }
@@ -181,33 +188,33 @@
             return sb.ToString();
         }
 
-        private string GenerateReport(Dictionary<string, NodeInfo> keysProd, Dictionary<string, NodeInfo> keysUAT, CompareResult result)
+        private string GenerateReport(Dictionary<string, NodeInfo> keysConfig1, Dictionary<string, NodeInfo> keysConfig2, CompareResult result)
         {
             var sb = new StringBuilder();
 
-            sb.AppendLine("\nNodes that are present only in prod config file\n");
-            foreach (var node in result.OnlyOnProd)
+            sb.AppendLine("\nNodes that are present only in config 1 file\n");
+            foreach (var node in result.OnlyInConfig1)
             {
                 sb.AppendLine(node);
-                sb.Append(GenerateNodeReport(keysProd[node]));
+                sb.Append(GenerateNodeReport(keysConfig1[node]));
             }
 
-            sb.AppendLine("\nNodes that are present only in UAT config file\n");
-            foreach (var node in result.MissedOnProd)
+            sb.AppendLine("\nNodes that are present only in config 2 file\n");
+            foreach (var node in result.MissedInConfig1)
             {
                 sb.AppendLine(node);
-                sb.Append(GenerateNodeReport(keysUAT[node]));
+                sb.Append(GenerateNodeReport(keysConfig2[node]));
             }
 
             sb.AppendLine("\nNodes that are present in both config files but with different attributes\n");
-            foreach (var node in result.ChangedOnProd)
+            foreach (var node in result.ChangedInConfig1)
             {
                 sb.AppendLine(node);
 
-                if (!string.IsNullOrEmpty(keysUAT[node].Value))
-                    sb.AppendFormat("\tvalue = {0}\n", keysUAT[node].Value);
+                if (!string.IsNullOrEmpty(keysConfig2[node].Value))
+                    sb.AppendFormat("\tNode value = {0}\n", keysConfig2[node].Value);
                 foreach (var change in result.Changes[node])
-                    sb.AppendFormat("\t{0} (prod)\"{1}\" (UAT)\"{2}\"\n", change.Attribute, change.ValueOnProd, change.ValueOnUAT);
+                    sb.AppendFormat("\t(1) {0}=\"{1}\"\n\t(2) {0}=\"{2}\"\n", change.Attribute, change.ValueInConfig1, change.ValueInConfig2);
                 if (result.Changes[node].Any())
                     sb.Append("\n");
             }
@@ -219,17 +226,17 @@
         {
             lblStatus.Text = @"Comparing...";
 
-            var keysProd = XMLToDictionary(tbProd.Text);
-            var keysUAT = XMLToDictionary(tbUAT.Text);
-            var result = Compare(keysProd, keysUAT);
-            var report = GenerateReport(keysProd, keysUAT, result);
+            var keysConfig1 = XMLToDictionary(tbConfig1.Text);
+            var keysConfig2 = XMLToDictionary(tbConfig2.Text);
+            var result = Compare(keysConfig1, keysConfig2);
+            var report = GenerateReport(keysConfig1, keysConfig2, result);
 
             lblStatus.Text = @"Saving report...";
 
             try
             {
                 var file = File.CreateText(tbResult.Text);
-                file.WriteLine("Comparison result between {0} (prod) and {1} (UAT)", tbProd.Text, tbUAT.Text);
+                file.WriteLine("Comparison result between {0} (1) and {1} (2)", tbConfig1.Text, tbConfig2.Text);
                 file.Write(report);
                 file.Flush();
                 file.Close();
